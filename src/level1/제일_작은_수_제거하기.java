1ac1da324c91902bc0d79f03c9c206d07386b3ae
Hosae0905 <didghtls11@gmail.com>
2023-11-25 00:03:27 +0900

    [Feat] 11-24 알고리즘 문제 풀이 프로그래머스 레벨 1단계 문제 풀이

---
+++ src/level1/제일_작은_수_제거하기.java
@@ -0,0 +1,40 @@
+package level1;
+
+public class 제일_작은_수_제거하기 {
+    public static int[] solution(int[] arr) {
+
+        if (arr.length == 1) {
+            int[] answer = {-1};
+            return answer;
+        }
+
+        int min = arr[0];
+        int[] answer = new int[arr.length - 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (min == arr[i]) {
+                continue;
+            } else {
+                answer[count++] = arr[i];
+            }
+        }
+
+        return answer;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = { 4, 3, 2, 1 };
+        int[] solution = solution(arr);
+        for (int i = 0; i < solution.length; i++) {
+            System.out.println(solution[i]);
+        }
+    }
+}
