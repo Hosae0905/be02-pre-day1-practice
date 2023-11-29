704e5ddb6e27838ac6275c0d1f02fa57dc8c3bb2
Hosae0905 <didghtls11@gmail.com>
2023-11-29 23:30:01 +0900

    [Feat] 11-29 알고리즘 문제 풀이 프로그래머스 스택과 큐 문제 풀이

---
+++ src/stack/올바른_괄호.java
@@ -0,0 +1,28 @@
+package stack;
+
+import java.util.Stack;
+
+public class 올바른_괄호 {
+    static boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == '(') {
+                stack.push('(');
+            } else if (charArray[i] == ')') {
+                if (stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+
+    public static void main(String[] args) {
+        boolean solution = solution("(())()");
+        System.out.println("solution = " + solution);
+    }
+}
---
+++ src/stack/프로세스.java
@@ -0,0 +1,54 @@
+package stack;
+
+import java.util.*;
+
+public class 프로세스 {
+    public static int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        /*Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(priorities[i]);
+        }
+
+
+        for (int i = 0; i < priorities.length; i++) {
+            int max = queue.poll();
+            if (max < queue.peek()) {
+                queue.add(max);
+                max = queue.poll();
+            } else {
+                queue.add(max);
+                list.add(i);
+            }
+        }*/
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int num : priorities) {
+            priorityQueue.add(num);
+        }
+
+        while (!priorityQueue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == priorityQueue.peek()) {
+                    priorityQueue.poll();
+                    answer++;
+                    if (i == location) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1,1,9,1,1,1 };
+        int location = 0;
+        int solution = solution(arr, location);
+        System.out.println("solution = " + solution);
+    }
+}
