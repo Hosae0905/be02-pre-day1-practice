56f59abd887b181848029b95804e7a8552e58dcb
Hosae0905 <didghtls11@gmail.com>
2023-12-04 23:29:37 +0900

    [Feat] 12-04 자료구조 공부 정리 PriorityQueue 공부 정리

---
+++ src/stack/PriorityQueueTest.java
@@ -0,0 +1,22 @@
+package stack;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class PriorityQueueTest {
+    public static void main(String[] args) {
+//        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+
+        queue.add(1);
+        queue.add(3);
+        queue.add(7);
+        queue.add(2);
+        queue.add(5);
+        queue.add(6);
+
+
+        System.out.println(queue.toString());
+    }
+}
