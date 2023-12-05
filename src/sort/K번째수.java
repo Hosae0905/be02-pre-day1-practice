eccbd790b7123d50e38966770a84be81a44f99f6
Hosae0905 <didghtls11@gmail.com>
2023-12-05 22:41:35 +0900

    [Feat] 12-05 프로그래머스 정렬 풀이 프로그래머스 k번째 수 문제 풀이

---
+++ src/sort/K번째수.java
@@ -0,0 +1,29 @@
+package sort;
+
+import java.util.Arrays;
+
+public class K번째수 {
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = new int[commands[i][1] - (commands[i][0] - 1)];
+            for (int j = 0; j < temp.length; j++) {
+                temp[j] = array[j + (commands[i][0] - 1)];
+            }
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 5, 2, 6, 3, 7, 4 };
+        int[][] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+
+        int[] solution = solution(arr, commands);
+        for (int i = 0; i < solution.length; i++) {
+            System.out.println(solution[i]);
+        }
+    }
+}
--- src/stack/다리를_지나는_트럭.java
+++ src/stack/다리를_지나는_트럭.java
@@ -26,36 +26,36 @@ public class 다리를_지나는_트럭 {
 
         Queue<Integer> queue = new LinkedList<>();
         int sum = 0;
-        int count = 0;
+        int count = 0;      // 트럭이 다리를 지나간 시간
 
-        for (int time : truck_weights) {
+        for (int time : truck_weights) {        // 현재 트럭은 4대
             while (true) {
                 if (queue.isEmpty()) {
                     queue.add(time);
-                    sum += time;
-                    count++;
+                    sum += time;                // 처음 트럭이 다리를 건널 때(무게 7인 트럭)
+                    count++;                    // 시간 + 1
                     break;
-                } else if (queue.size() == bridge_length) {
-                    sum -= queue.poll();
+                } else if (queue.size() == bridge_length) {     // 현재 다리에 있는 트럭이 총 다리의 길이와 같을 때
+                    sum -= queue.poll();        // 다리에서 트럭을 하나 빼주기
                 } else {
-                    if (sum + time > weight) {
-                        queue.add(0);
-                        count++;
+                    if (sum + time > weight) {      // 다리를 건너고 있는 트럭과 다음 건너는 순서인 트럭의 무게를 합쳤을 때 다리가 견딜 수 있는 무게보다 클 경우
+                        queue.add(0);               // 다리에 트럭을 올리지 않고 그냥 0을 추가
+                        count++;                    // 시간 + 1
                     } else {
-                        queue.add(time);
-                        sum += time;
-                        count++;
+                        queue.add(time);            // 다리를 건너고 있는 트럭과 다음 건너는 순서인 트럭의 무게를 합쳤을 때 다리가 견딜 수 있는 무게보다 작을 경우
+                        sum += time;                // 현재 다리를 건너고 있는 트럭의 무게에 새로 다리를 건너는 트럭의 무게를 더하기
+                        count++;                    // 시간 + 1
                         break;
                     }
                 }
             }
         }
-        return count + bridge_length;
+        return count + bridge_length;           // 마지막으로 다리를 건너는 트럭은 다리의 길이 만큼 시간이 걸리기 때문에 더해준다.
     }
 
     public static void main(String[] args) {
-        int[] arr = {10};
-        int solution = solution(100, 100, arr);
+        int[] arr = {7, 4, 5, 6};
+        int solution = solution(2, 10, arr);
         System.out.println("solution = " + solution);
     }
 }
